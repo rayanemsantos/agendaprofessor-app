aa5907baa16645c846b557abb060f742fab6a143
Ravi Silva <ravifelipee@gmail.com>
2022-11-06 16:13:19 -0300

    Adicionando imagem de perfil e o icone de edição da imagem de perfil

--- src/pages/perfil/PerfilPage.js
+++ src/pages/perfil/PerfilPage.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+// import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Container from "../../components/container";
 import { Subtitle, Title } from "../../components/text";
@@ -16,14 +16,18 @@ import {
   Keyboard,
   Image,
 } from "react-native";
+
 import { URL_BASE } from "../../providers/config";
 import { CustomButtonContained } from "../../components/customButton";
-
 import { getToken, getUserData } from "../../storage/Storage";
 import { AppInput } from "../../components/customInput";
-import HomePage from "../home/HomePage";
+import ImagemPerfil from "../../assets/imagem-perfil.png";
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function PerfilPage() {
+
+  
   return (
     <ScrollView>
       <Container>
@@ -32,40 +36,54 @@ export default function PerfilPage() {
             <Title text="Dados Pessoais" />
           </View>
 
+          <View>
+            <Image source={ImagemPerfil} style={styles.formImage} />
+            <Icon name="edit" size={30} style={styles.formIcon}  />
+          </View>
+
           <View style={styles.formContainer}>
-            <AppInput label="Nome" name="name" placeholder="Insira seu nome" />
+            <AppInput
+              label="Nome"
+              name="name"
+              value="CAMILA DE OLIVEIRA MEDEIROS"
+              placeholder="Insira seu nome"
+            />
 
             <AppInput
               label="E-mail"
+              value="camifet@email.com.br"
               name="email"
               placeholder="Insira seu e-mail"
             />
 
-            <AppInput label="CPF" name="cpf" placeholder="Insira seu cpf" />
+            <AppInput
+              label="CPF"
+              name="cpf"
+              value="022.242.292-00"
+              placeholder="Insira seu cpf"
+            />
 
             <AppInput
               label="Escolaridade"
               name="escolaridade"
+              value="ESPECIALISTA"
               placeholder="Insira sua escolaridade"
             />
 
             <AppInput
               label="PIS/PASEP"
               name="PIS/PASEP"
+              value="156224678"
               placeholder="Insira seu PIS/PASEP"
             />
 
             <AppInput
               label="Cidade"
               name="cidade"
+              value="Fortaleza, Ceará"
               placeholder="Insira sua cidade"
             />
-            <CustomButtonContained
-            style={styles.formButton}
-              text="Alterar"
-              
-            />
-  
+            <CustomButtonContained style={styles.formButton} text="Alterar" />
           </View>
         </ScrollView>
       </Container>
@@ -81,6 +99,19 @@ const styles = StyleSheet.create({
   },
   formButton: {
     marginBottom: 10,
-    marginTop: 10
+    marginTop: 10,
+  },
+  formImage: {
+    width: 130,
+    height: 130,
+    marginTop: 20,
+    marginBottom: 5,
+    borderRadius: 20,
+    alignSelf: "center"
+  },
+  formIcon: {
+    alignSelf: "center",
+    marginBottom: 10,
+    color: "#30009C"
   }
 });
